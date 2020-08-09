a3c9fdf1666a98a7ac614f32bdc1c2d7bf330dcc
Carla Reis <carla.jusreis@gmail.com>
2020-08-09 02:33:28 -0400

    SegundoCommit

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -4,10 +4,10 @@ const Category = require("./category");
 const slugify =require("slugify")
 
 router.get("/admin/categories/new",(req, res) => {
-    res.render("admin/categories/new");
+    res.render("/views/admin/categories/new.ejs");
 })
 
-router.get("/categories/save", (req,res) => {
+router.post("/categories/save", (req,res) => {
     var title = req.body.title;
     if(title != undefined){
         Category.create({
@@ -17,7 +17,32 @@ router.get("/categories/save", (req,res) => {
             res.redirect("/");
         })
     } else{
-        res.redirect("/admin/categories/new");
+        res.redirect('/admin/categories/new');
+    }
+})
+
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", {categories: categories});
+    })
+})
+
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id
+    if(id != undefined){
+        if(!isNaN(id)){
+
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect('/admin/categories')
+            })
+
+        }else{
+            res.redirect('/admin/categories/')
+        }
     }
 })
 
