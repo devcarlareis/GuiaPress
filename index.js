9d83bd1187aa91d80429d06ca78f29e5fb2436b0
Carla Reis <carla.jusreis@gmail.com>
2020-08-12 05:10:20 -0400

    homepage

--- index.js
+++ index.js
@@ -32,8 +32,10 @@ app.use("/",CategoriesController);
 app.use("/",ArticlesController);
 
 app.get("/",(req,res) => {
-    res.render("index");
-})
+    Article.findAll().then(articles =>{
+        res.render("index", {articles: articles});
+    })
+});
 
 app.listen(8080,() => {
     console.log("O servidor está rodando!")
